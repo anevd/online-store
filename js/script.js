fa7803a0322010c04b7a1b077159b5fe525e3115
mkarsums15@mail.ru <mkarsums15@mail.ru>
2023-04-11 17:27:18 +0300

    add a function to remove an item from the cart, refactoring

--- js/script.js
+++ js/script.js
@@ -1,93 +1,214 @@
+const data = [
+	{
+		id: 0,
+		src: "./img/chair.jpeg",
+		title: "Pica Chair",
+		color: "Jade Velvet",
+		"general dimensions": '29" L x 30" W x 27" H',
+		price: "$690",
+	},
+	{
+		id: 1,
+		src: "./img/lounger.jpeg",
+		title: "Range 4-Piece Sectional Lounger",
+		color: "Heather Charcoal",
+		"general dimensions": '91.5" L x 60.5" D x 28" H',
+		price: "$3405",
+	},
+	{
+		id: 2,
+		src: "./img/sideboard.jpeg",
+		title: "Carta Credenza",
+		color: "Walnut",
+		"general dimensions": '50" L x 16.5" W x 28" H',
+		price: "$395",
+	},
+	{
+		id: 3,
+		src: "./img/ottoman.jpeg",
+		title: "Range Ottoman",
+		color: "Heather Charcoal",
+		"general dimensions": '29.5" L x 29.5" D x 16" H',
+		price: "$600",
+	},
+	{
+		id: 4,
+		src: "./img/lamp.jpeg",
+		title: "Ellis Floor Lamp",
+		color: "Walnut",
+		"general dimensions": '16" Dia. x 11" H',
+		price: "$190",
+	},
+	{
+		id: 5,
+		src: "./img/chorus-bed.jpeg",
+		title: "Chorus Bed, Queen with Wood Headboard",
+		color: "Walnut",
+		"general dimensions": '85" D x 63" W x 14.5" H',
+		price: "$1260",
+	},
+	{
+		id: 6,
+		src: "./img/lodge-chair.jpeg",
+		title: "Lodge Chair",
+		color: "Camel Leather",
+		"general dimensions": '29" L x 31" W x 33" H',
+		price: "$760",
+	},
+	{
+		id: 7,
+		src: "./img/prospect-nightstand.jpeg",
+		title: "Prospect Nightstand",
+		color: "Walnut",
+		"general dimensions": '20" L x 17.7" D x 24" H',
+		price: "$395",
+	},
+	{
+		id: 8,
+		src: "./img/index-stool.jpeg",
+		title: "Index Stool",
+		color: "Walnut",
+		"general dimensions": '13.5" Dia. x 18" H',
+		price: "$195",
+	},
+];
+
+const dataOrdered = [];
+
+const orderItemTemplate = `
+<div class="cart-item">
+						<div class="cart-item__img"><img src="" class="cart-item__img-src" alt="" /></div>
+						<div class="cart-item__content">
+							<div>
+								<div class="cart-item__title"></div>
+								<div class="cart-item__color"></div>
+								<div class="cart-item__general-dimensions"></div>
+							</div>
+							<div class="cart-item__price">
+								<div class="cart-item__price_default"></div>
+							</div>
+						</div>
+						<button class="cart-item__trash">
+					<svg class="cart-item__trash-svg" fill="#000000" width="800px" height="800px" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+						<path
+							d="M22,5a1,1,0,0,1-1,1H3A1,1,0,0,1,3,4H8V3A1,1,0,0,1,9,2h6a1,1,0,0,1,1,1V4h5A1,1,0,0,1,22,5ZM4.934,21.071,4,8H20l-.934,13.071a1,1,0,0,1-1,.929H5.931A1,1,0,0,1,4.934,21.071ZM15,18a1,1,0,0,0,2,0V12a1,1,0,0,0-2,0Zm-4,0a1,1,0,0,0,2,0V12a1,1,0,0,0-2,0ZM7,18a1,1,0,0,0,2,0V12a1,1,0,0,0-2,0Z" />
+					</svg>
+				</button>
+					</div>`;
+
+let catalog = document.querySelector(".catalog__wrapper");
 const total = document.querySelector(".purchase__total-price");
 let order = [];
-const button = document.querySelector(".button");
+const buttonCart = document.querySelector(".button");
 let itemsCount = document.querySelector(".shopping-cart__count");
 let discountCounter = 0;
 const discountMessage = document.createElement("div");
-let result = 0;
 let orderArea = document.querySelector(".shopping-cart__order");
 
-function createOrderList() {
-	localStorage.setItem("orderFull", JSON.stringify(order));
+window.onload = function () {
+	if (window.location.href.indexOf("cart.html") > -1) {
+		order = JSON.parse(localStorage.getItem("orderFull"));
+		countOrderItems();
+		showInCart();
+		showTotalPrice();
+		buttonCart.addEventListener("click", useDiscount);
+	}
+	if (window.location.href.indexOf("index.html") > -1) {
+		showCatalog();
+		let buttonsToAdd = document.querySelectorAll(".catalog-item__button");
+		buttonsToAdd.forEach((button) => {
+			button.addEventListener("click", addToCart);
+		});
+		createOrderList();
+	}
+};
+
+function showCatalog() {
+	data.forEach((el) => {
+		catalog.innerHTML += `
+	<div class="catalog-item">
+						<div class="catalog-item__img"><img src='${el["src"]}'class="catalog-item__img-src" alt='${el["title"]}'/></div>
+						<div class="catalog-item__content">
+							<div>
+								<div class="catalog-item__title">${el["title"]}</div>
+								<div class="catalog-item__color">Color: ${el["color"]}</div>
+								<div class="catalog-item__general-dimensions">General Dimensions: ${el["general dimensions"]}</div>
+							</div>
+							<div class="catalog-item__price">
+								<div class="catalog-item__price_default">${el["price"]}</div>
+							</div>
+						</div>
+						<button class="catalog-item__button" data-num="${el["id"]}">Add to Cart</button>
+					</div>
+	`;
+	});
 }
 
-function showTotalPrice() {
-	let sum = 0;
-	for (let i = 0; i < orderArea.childElementCount; i++) {
-		let price = +order[i]["Price"].slice(1);
-		sum += price;
-	}
-	total.innerHTML = "$" + sum;
+const addToCart = (e) => {
+	e.target.style.display = "none";
+	let buttonAdded = document.createElement("button");
+	buttonAdded.className = "catalog-item__button_added";
+	buttonAdded.innerHTML = "Added";
+	e.target.after(buttonAdded);
+	dataOrdered.push(data[e.target.dataset.num]);
+};
+
+function createOrderList() {
+	localStorage.setItem("orderFull", JSON.stringify(dataOrdered));
 }
 
 function countOrderItems() {
 	itemsCount.innerHTML = order.length + " items";
 }
 
-function ShowInCart() {
-	order.forEach((el) => {
-		let OrderItem = document.createElement("div");
-		OrderItem.className = "cart-item";
-		let OrderItemImg = document.createElement("div");
-		OrderItemImg.className = "cart-item__img";
-		OrderItem.append(OrderItemImg);
-		let OrderItemImgSrc = document.createElement("img");
-		OrderItemImgSrc.className = "cart-item__img-src";
-		OrderItemImgSrc.src = el["Picture"];
-		OrderItemImg.append(OrderItemImgSrc);
-		let OrderItemContent = document.createElement("div");
-		OrderItemContent.className = "cart-item__content";
-		OrderItem.append(OrderItemContent);
-		let OrderItemContentDiv = document.createElement("div");
-		OrderItemContent.append(OrderItemContentDiv);
-		let OrderItemTitle = document.createElement("div");
-		OrderItemTitle.className = "cart-item__title";
-		OrderItemTitle.innerHTML = el["Title"];
-		OrderItemContentDiv.append(OrderItemTitle);
-		let OrderItemColor = document.createElement("div");
-		OrderItemColor.className = "cart-item__color";
-		OrderItemColor.innerHTML = el["Color"];
-		OrderItemContentDiv.append(OrderItemColor);
-		let OrderItemDimensions = document.createElement("div");
-		OrderItemDimensions.className = "cart-item__general-dimensions";
-		OrderItemDimensions.innerHTML = el["General dimensions"];
-		OrderItemContentDiv.append(OrderItemDimensions);
-		let OrderItemPrice = document.createElement("div");
-		OrderItemPrice.className = "cart-item__price";
-		OrderItemContent.append(OrderItemPrice);
-		let OrderItemPriceDefault = document.createElement("div");
-		OrderItemPriceDefault.className = "cart-item__price_default";
-		OrderItemPriceDefault.innerHTML = el["Price"];
-		OrderItemPrice.append(OrderItemPriceDefault);
-		orderArea.append(OrderItem);
+function showTotalPrice() {
+	let sum = 0;
+	for (let i = 0; i < orderArea.childElementCount; i++) {
+		let price = +order[i]["price"].slice(1);
+		sum += price;
+	}
+	total.innerHTML = "$" + sum;
+}
+
+function showInCart() {
+	orderArea.innerHTML = "";
+	order.forEach((el, index) => {
+		orderArea.innerHTML += `
+		<div class="cart-item">
+								<div class="cart-item__img"><img src="${el["src"]}" class="cart-item__img-src" alt="${el["title"]}" /></div>
+								<div class="cart-item__content">
+									<div>
+										<div class="cart-item__title">${el["title"]}</div>
+										<div class="cart-item__color">Color: ${el["color"]}</div>
+										<div class="cart-item__general-dimensions">General Dimensions: ${el["general dimensions"]}</div>
+									</div>
+									<div class="cart-item__price">
+										<div class="cart-item__price_default">${el["price"]}</div>
+									</div>
+								</div>
+								<button class="cart-item__trash" data-num='${index}'>
+							<svg class="cart-item__trash-svg" fill="#000000" width="800px" height="800px" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+								<path
+									d="M22,5a1,1,0,0,1-1,1H3A1,1,0,0,1,3,4H8V3A1,1,0,0,1,9,2h6a1,1,0,0,1,1,1V4h5A1,1,0,0,1,22,5ZM4.934,21.071,4,8H20l-.934,13.071a1,1,0,0,1-1,.929H5.931A1,1,0,0,1,4.934,21.071ZM15,18a1,1,0,0,0,2,0V12a1,1,0,0,0-2,0Zm-4,0a1,1,0,0,0,2,0V12a1,1,0,0,0-2,0ZM7,18a1,1,0,0,0,2,0V12a1,1,0,0,0-2,0Z" />
+							</svg>
+						</button>
+							</div>`;
+	});
+	let orderButton = document.querySelectorAll(".cart-item__trash");
+	orderButton.forEach((button) => {
+		button.addEventListener("click", deleteOrderedItem);
 	});
 }
 
-function addToCart(card) {
-	const buttonToAdd = card.querySelector(".catalog-item__button");
-	buttonToAdd.style.display = "none";
-	let buttonAdded = document.createElement("button");
-	buttonAdded.className = "catalog-item__button_added";
-	buttonAdded.innerHTML = "Added";
-	card.append(buttonAdded);
-	let cartItem = {};
-	cartItem["Picture"] = card.querySelector(".catalog-item__img-src").src;
-	cartItem["Title"] = card.querySelector(".catalog-item__title").innerText;
-	cartItem["Color"] = card.querySelector(".catalog-item__color").innerText;
-	cartItem["General dimensions"] = card.querySelector(".catalog-item__general-dimensions").innerText;
-	cartItem["Price"] = card.querySelector(".catalog-item__price_default").innerText;
-	localStorage.setItem(cartItem, JSON.stringify(cartItem));
-	order.push(cartItem);
-}
-
-function useDiscount() {
+const useDiscount = (e) => {
+	let result = 0;
 	let pricesDiscount = document.querySelectorAll(".cart-item__price_default");
-	if (discountCounter === 1) {
+	if (discountCounter === 1 && discountMessage.innerHTML !== "Cart is empty") {
 		discountMessage.innerHTML = "You have already used the discount";
 	}
 	if (discountCounter === 0) {
 		for (let i = 0; i < orderArea.childElementCount; i++) {
-			let price = order[i]["Price"].slice(1);
+			let price = order[i]["price"].slice(1);
 			let priceDiscount = price * (1 - 0.2);
 			let discountDiv = document.createElement("div");
 			discountDiv.className = "cart-item__price_discount";
@@ -97,18 +218,22 @@ function useDiscount() {
 			result += priceDiscount;
 		}
 		discountMessage.className = "purchase__discount-message";
-		discountMessage.innerHTML = "Discount successfully used";
-		button.after(discountMessage);
+		if (result === 0) {
+			discountMessage.innerHTML = "Cart is empty";
+		} else {
+			discountMessage.innerHTML = "Discount successfully used";
+		}
+		e.target.after(discountMessage);
 		total.innerHTML = "$" + result;
 		discountCounter = 1;
 	}
-}
+};
 
-window.onload = function () {
-	if (window.location.href.indexOf("cart.html") > -1) {
-		order = JSON.parse(localStorage.getItem("orderFull"));
-		countOrderItems();
-		ShowInCart();
-		showTotalPrice();
-	}
+const deleteOrderedItem = (e) => {
+	order.splice(e.currentTarget.dataset.num, 1);
+	discountCounter = 0;
+	discountMessage.innerHTML = "";
+	showInCart();
+	countOrderItems();
+	showTotalPrice();
 };
